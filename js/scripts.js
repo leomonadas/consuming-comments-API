30eb5ac5280a99d7713beb8b20cfb4432d000cb1
Leonardo Alves <77860170+leomonadas@users.noreply.github.com>
2023-07-21 17:16:30 -0300

    feat: comments section

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 const url = "https://jsonplaceholder.typicode.com/posts";
-
+//Homepage
 const loadingElement = document.getElementById("loading");
 const postsContainer = document.getElementById("posts-container");
 
+// Posts pages
+const postPage = document.querySelector("#post");
+const postContainer = document.querySelector("#post-container");
+const commentscontainer = document.querySelector("#comments-container");
 
+// Get id from url
+const urlSearchParams = new URLSearchParams(window.location.search);
+const postId = urlSearchParams.get("id");
+
+
+// Get all posts
 async function getAllPosts(){
     const response = await fetch(url);
 
@@ -32,4 +42,48 @@ async function getAllPosts(){
     })
 }
 
-getAllPosts();
+// Get individual post
+async function getPost(id) {
+    const [responsePost, responseComments] = await Promise.all ([
+        fetch(`${url}/${id}`),
+        fetch(`${url}/${id}/comments`)
+    ])
+
+    const dataPost = await responsePost.json();
+    const dataComments = await responseComments.json();
+
+    console.log(dataComments)
+
+    loadingElement.classList.add("hide");
+    postPage.classList.remove("hide");
+
+    const title = document.createElement("h1");
+    const body = document.createElement("p");
+
+    title.innerText = dataPost.title;
+    body.innerText = dataPost.body;
+
+    postsContainer.appendChild(title);
+    postsContainer.appendChild(body);
+
+    //Comments section
+    dataComments.map((comment) => {
+        const name = document.createElement("h2");
+        const email = document.createElement("h3");
+        const body = document.createElement("p");
+
+        name.innerText = comment.name;
+        email.innerText = comment.email;
+        body.innerText = comment.body;
+
+        commentscontainer.appendChild(name);
+        commentscontainer.appendChild(email);
+        commentscontainer.appendChild(body);
+    })
+}
+
+if (!postId) {
+    getAllPosts();
+} else {
+    getPost(postId);
+}
